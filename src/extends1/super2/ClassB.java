652b0bf40b45577ef4b6e24926a7e9e536ada84a
sujin kim <mh030128@naver.com>
2025-02-24 14:54:08 +0900

    inheritanceEx

--- src/extends1/super2/ClassB.java
+++ src/extends1/super2/ClassB.java
@@ -3,8 +3,10 @@ package extends1.super2;
 public class ClassB extends ClassA{
 
     public ClassB(int a) {
-        super();    // 기본생성자. 생략가능(기본생성자인 경우에만.)
+        super();
         System.out.println("ClassB 생성자 a = " + a);
+        /*this(100, 0);    // 기본생성자. 생략가능(기본생성자인 경우에만.)
+        System.out.println("ClassB 생성자 a = " + a);*/
     }
 
     public ClassB(int a, int b) {
--- src/extends1/super2/Super2Main.java
+++ src/extends1/super2/Super2Main.java
@@ -4,6 +4,7 @@ public class Super2Main {
     public static void main(String[] args) {
 
         ClassC classC = new ClassC();
+        //ClassB classB = new ClassB(100);
 
     }
 }
